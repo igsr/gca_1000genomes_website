5e3355e0b7742723002cc12128145796adfd1a8c
streeter <streeter@ebi.ac.uk>
2017-01-17 14:05:04 +0000

    Data portal beta logo

--- _data-portal/app/app.component.ts
+++ _data-portal/app/app.component.ts
@@ -1,7 +1,27 @@
 import { Component } from '@angular/core';
 
+let appComponentStyles: string = `
+div.data-portal-section {
+  position: relative;
+}
+h3.data-portal-beta {
+  position: absolute;
+  top: 0;
+  right: 0;
+  background-color: #262626;
+  color: #DAA406;
+  padding: 15px;
+  margin: 0px;
+  border-bottom-left-radius: 4px;
+}
+span.beta {
+  color: #FFF;
+}
+`;
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
+    styles: [ appComponentStyles ],
 })
 export class AppComponent{};
