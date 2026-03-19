b2aa0a6e6c789069de05ddb029d182f84075d465
Aine Fairbrother-Browne <42773574+ainefairbrother@users.noreply.github.com>
2026-03-19 15:30:22 +0000

    fix: map error code to message text

--- _data-portal/app/shared/components/api-error.component.ts
+++ _data-portal/app/shared/components/api-error.component.ts
@@ -47,7 +47,23 @@ export class ApiErrorComponent implements OnInit{
   }
 
   get bannerTitle(): string {
-    return 'Data Portal Temporarily Unavailable';
+    if (this.errors.length === 0) {
+      return '';
+    }
+
+    if (this.isTooManyRequestsError(this.errors[0].error)) {
+      return 'Too Many Requests';
+    }
+
+    if (this.isNotFoundError(this.errors[0].error)) {
+      return 'Requested Item Not Found';
+    }
+
+    if (this.isPortalUnavailableError(this.errors[0].error)) {
+      return 'Data Portal Temporarily Unavailable';
+    }
+
+    return 'Request Could Not Be Completed';
   }
 
   get bannerMessage(): string {
@@ -62,4 +78,16 @@ export class ApiErrorComponent implements OnInit{
     this.apiErrorService.clearErrors();
     return;
   };
+
+  private isTooManyRequestsError(message: string): boolean {
+    return message === 'Too many requests are coming from your network. Please wait a moment and try again.';
+  }
+
+  private isNotFoundError(message: string): boolean {
+    return message === 'The requested page or record could not be found.';
+  }
+
+  private isPortalUnavailableError(message: string): boolean {
+    return message === 'Apologies, the data portal is temporarily unavailable. We are working to get it back up.';
+  }
 }
